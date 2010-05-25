c2014d55e3518f14265c4fc2a1f1d190aab414ea
Michael Stephens <me@mikej.st>
2010-05-25 17:29:25 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from scrapelib import __version__
+
+long_description = open('README.rst').read()
+
+setup(name="scrapelib",
+      version=__version__,
+      py_modules=['scrapelib'],
+      author="Michael Stephens",
+      author_email='mstephens@sunlightfoundation.com',
+      license="BSD",
+      url="http://github.com/mikejs/scrapelib",
+      long_description=long_description,
+      platforms=["any"],
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      install_requires=["httplib2 >= 0.6.0"]
+      )
