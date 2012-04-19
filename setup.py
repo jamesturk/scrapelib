b067fceff634b056f18fe4f1f91838d8f8356c4e
James Turk <james.p.turk@gmail.com>
2012-04-19 16:20:42 -0400

    change dependency for py3 to chardet2

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
+import sys
 from setuptools import setup
-from scrapelib import __version__
 
 long_description = open('README.rst').read()
 
+required = ['httplib2>=0.7.0']
+if sys.version_info[0] > 2:
+    required.append('chardet2')
+else:
+    required.append('chardet')
+
 setup(name="scrapelib",
-      version=__version__,
+      version='0.6.0',
       py_modules=['scrapelib'],
       author="James Turk",
       author_email='jturk@sunlightfoundation.com',
@@ -23,7 +29,7 @@ setup(name="scrapelib",
                    ("Topic :: Software Development :: Libraries :: "
                     "Python Modules"),
                    ],
-      install_requires=["httplib2 >= 0.7.0", "chardet"],
+      install_requires=required,
       entry_points="""
 [console_scripts]
 scrapeshell = scrapelib:scrapeshell
