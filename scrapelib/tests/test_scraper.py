f0fcc5e0a799b948bb537dc022d163b3797fd987
James Turk <dev@jamesturk.net>
2022-11-06 18:56:03 -0600

    ignore extra parameter retry_on_404 for now

--- scrapelib/tests/test_scraper.py
+++ scrapelib/tests/test_scraper.py
@@ -259,7 +259,7 @@ def test_retry_404() -> None:
     )
 
     with mock.patch.object(requests.Session, "request", mock_request):
-        resp = s.get("http://dummy/", retry_on_404=True)
+        resp = s.get("http://dummy/", retry_on_404=True)  # type: ignore
     assert mock_request.call_count == 2
     assert resp.status_code == 200
 
@@ -270,13 +270,13 @@ def test_retry_404() -> None:
 
     # retry on 404 true, 4 tries
     with mock.patch.object(requests.Session, "request", mock_request):
-        resp = s.get("http://dummy/", retry_on_404=True)
+        resp = s.get("http://dummy/", retry_on_404=True)  # type: ignore
     assert resp.status_code == 404
     assert mock_request.call_count == 4
 
     # retry on 404 false, just one more try
     with mock.patch.object(requests.Session, "request", mock_request):
-        resp = s.get("http://dummy/", retry_on_404=False)
+        resp = s.get("http://dummy/", retry_on_404=False)  # type: ignore
     assert resp.status_code == 404
     assert mock_request.call_count == 5
 
@@ -287,7 +287,7 @@ def test_retry_ssl() -> None:
     # ensure SSLError is considered fatal even w/ retries
     with mock.patch.object(requests.Session, "request", mock_sslerror):
         with pytest.raises(requests.exceptions.SSLError):
-            s.get("http://dummy/", retry_on_404=True)
+            s.get("http://dummy/", retry_on_404=True)  # type: ignore
     assert mock_sslerror.call_count == 1
 
 
@@ -408,7 +408,7 @@ def test_ftp_retries() -> None:
     # retry on
     with mock.patch("scrapelib.urllib_urlopen", mock_urlopen):
         s = Scraper(retry_attempts=2, retry_wait_seconds=0.001)
-        r = s.get("ftp://dummy/", retry_on_404=True)
+        r = s.get("ftp://dummy/", retry_on_404=True) # type: ignore
         assert r.content == b"ftp success!"
     assert mock_urlopen.call_count == 2
 
